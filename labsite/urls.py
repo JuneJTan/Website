6606f5453ff45f7586a9f7f2d10e23fa8aaea5c8
JuuTon <jooTon708@outlook.com>
2026-06-26 21:51:39 +0800

    Add lagacy web pages and revise some discriptions

--- labsite/urls.py
+++ labsite/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, re_path
+from django.views.static import serve
 
 from cms.views import home
 
@@ -30,3 +31,6 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [
+        re_path(r'^wwwroot/(?P<path>.*)$', serve, {"document_root": settings.BASE_DIR / "wwwroot"}),
+    ]
